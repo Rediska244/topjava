93d8aad9e0349bcc94350814a549935b3dcdadcc
Andrei <rediska244@yandex.ru>
2017-11-15 17:50:55 +0300

    HW02

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -3,13 +3,14 @@ package ru.javawebinar.topjava.repository;
 import ru.javawebinar.topjava.model.Meal;
 
 import java.util.Collection;
+import java.util.Map;
 
 public interface MealRepository {
-    Meal save(Meal meal);
+    Meal save(int user, Meal meal);
 
-    void delete(int id);
+    void delete(int user, int id);
 
-    Meal get(int id);
+    Meal get(int user, int id);
 
-    Collection<Meal> getAll();
+    Collection<Meal> getAll(int user);
 }
--- src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
@@ -4,41 +4,65 @@ import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+import static ru.javawebinar.topjava.repository.mock.InMemoryUserRepositoryImpl.ADMIN_ID;
+import static ru.javawebinar.topjava.repository.mock.InMemoryUserRepositoryImpl.USER_ID;
 
 public class InMemoryMealRepositoryImpl implements MealRepository {
-    private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private Map<Integer, Map<Integer, Meal>> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
+    public static final Comparator<Meal> USER_MEAL_DATE_COMP = (u1, u2) -> u1.getDateTime().compareTo(u2.getDateTime());
+
     {
-        MealsUtil.MEALS.forEach(this::save);
+        MealsUtil.MEALS.forEach(um -> save(USER_ID, um));
+
+        save(ADMIN_ID, new Meal(LocalDateTime.of(2015, Month.JUNE, 1, 14, 0), "Админ ланч", 510));
+        save(ADMIN_ID, new Meal(LocalDateTime.of(2015, Month.JUNE, 1, 21, 0), "Админ ужин", 1500));
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(int user, Meal meal) {
+        Map<Integer, Meal> list = repository.get(user);
+        if (list == null) meal.setId(counter.incrementAndGet());
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
         }
-        repository.put(meal.getId(), meal);
+        list.put(meal.getId(), meal);
+        repository.put(user, list);
         return meal;
     }
 
     @Override
-    public void delete(int id) {
-        repository.remove(id);
+    public void delete(int user, int id) {
+        Map<Integer, Meal> list = repository.get(user);
+        list.remove(id);
+        if (list != null) repository.put(user, list);
+        else
+            repository.remove(user);
     }
 
     @Override
-    public Meal get(int id) {
-        return repository.get(id);
+    public Meal get(int user, int id) {
+//        return repository.get(id);
+        Map<Integer, Meal> list = repository.get(user);
+        if (list == null) return null;
+        return list.get(id);
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return repository.values();
+    public Collection<Meal> getAll(int user) {
+        if (repository.get(user) == null) return null;
+        return repository.get(user).values().stream().sorted(USER_MEAL_DATE_COMP).collect(Collectors.toList());
     }
 }
 
