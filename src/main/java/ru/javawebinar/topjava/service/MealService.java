49804ea9aafb5384edabf9ec8e30c887fac20b2d
Andrei <rediska244@yandex.ru>
2017-11-16 15:08:03 +0300

    3_0_fix.patch

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,4 +1,15 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
+
+import java.util.Collection;
+
 public interface MealService {
+    Meal save(int user, Meal meal);
+
+    void delete(int user, int id);
+
+    Meal get(int user, int id);
+
+    Collection<Meal> getAll(int user);
 }
--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -1,9 +1,31 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+
+import java.util.Collection;
 
 public class MealServiceImpl implements MealService {
 
     private MealRepository repository;
 
+    @Override
+    public Meal save(int user, Meal meal) {
+        return null;
+    }
+
+    @Override
+    public void delete(int user, int id) {
+
+    }
+
+    @Override
+    public Meal get(int user, int id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Meal> getAll(int user) {
+        return null;
+    }
 }
