d9a90c878dc57e833ca4088354f619c52dc37957
Andrei <rediska244@yandex.ru>
2017-11-17 12:00:56 +0300

    3_02_HW2_meal_layers.patch

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,15 +1,27 @@
 package ru.javawebinar.topjava.service;
 
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
-import java.util.Collection;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
 
 public interface MealService {
-    Meal save(int user, Meal meal);
+    Meal get(int id, int userId) throws NotFoundException;
 
-    void delete(int user, int id);
+    void delete(int id, int userId) throws NotFoundException;
 
-    Meal get(int user, int id);
+    default List<Meal> getBetweenDates(LocalDate startDate, LocalDate endDate, int userId) {
+        return getBetweenDateTimes(LocalDateTime.of(startDate, LocalTime.MIN), LocalDateTime.of(endDate, LocalTime.MAX), userId);
+    }
 
-    Collection<Meal> getAll(int user);
+    List<Meal> getBetweenDateTimes(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId);
+
+    List<Meal> getAll(int userId);
+
+    Meal update(Meal meal, int userId) throws NotFoundException;
+
+    Meal create(Meal meal, int userId);
 }
--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -1,31 +1,52 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
-import java.util.Collection;
+import java.time.LocalDateTime;
+import java.util.List;
 
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
+
+@Service
 public class MealServiceImpl implements MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
 
-    @Override
-    public Meal save(int user, Meal meal) {
-        return null;
+    @Autowired
+    public MealServiceImpl(MealRepository repository) {
+        this.repository = repository;
     }
 
     @Override
-    public void delete(int user, int id) {
-
+    public Meal get(int id, int userId) {
+        return checkNotFoundWithId(repository.get(id, userId), id);
     }
 
     @Override
-    public Meal get(int user, int id) {
-        return null;
+    public void delete(int id, int userId) {
+        checkNotFoundWithId(repository.delete(id, userId), id);
     }
 
     @Override
-    public Collection<Meal> getAll(int user) {
-        return null;
+    public List<Meal> getBetweenDateTimes(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
+        return repository.getBetween(startDateTime, endDateTime, userId);
+    }
+
+    @Override
+    public List<Meal> getAll(int userId) {
+        return repository.getAll(userId);
+    }
+
+    @Override
+    public Meal update(Meal meal, int userId) {
+        return checkNotFoundWithId(repository.save(meal, userId), meal.getId());
+    }
+
+    @Override
+    public Meal create(Meal meal, int userId) {
+        return repository.save(meal, userId);
     }
 }
